e794199aa6f06d105c53b5a61c25d535a4e264fa
Wickes1 <27881570+wickes1@users.noreply.github.com>
2024-04-03 15:04:45 -0400

    wip: product detailse page

--- inventory-portal/src/components/product-details.tsx
+++ inventory-portal/src/components/product-details.tsx
@@ -1,7 +1,8 @@
 import prisma from "@/lib/prisma";
 import { inventory } from "@prisma/client";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 async function ProductDetails({ product }: { product: inventory }) {
 	const image_url = await prisma.inventory_display_properties.findFirst({
@@ -13,7 +14,12 @@ async function ProductDetails({ product }: { product: inventory }) {
 		}
 	});
 	return (
-		<div>
+		<div className="flex flex-col items-center justify-center mt-5">
+			<Button variant="outline" asChild className="w-1/4">
+				<Link href={`/inventory/${product.item_code}/uploadImage`}>
+					Edit Item Photo
+				</Link>
+			</Button>
 			<div className="flex justify-center">
 				<Image
 					src={
@@ -22,14 +28,15 @@ async function ProductDetails({ product }: { product: inventory }) {
 							: "/inventory_default.png"
 					}
 					alt="inventory"
-					width={500}
-					height={500}
-					className="h-[500px] w-[500px] object-contain"
+					width={300}
+					height={300}
+					className="h-[300px] w-[300px] object-contain"
 				/>
 			</div>
-			<div className="flex justify-between p-1">
-				<p className=" text-gray-400">{product.item_code}</p>
-				<p className=" text-gray-400">{product.bar_code}</p>
+			<hr className="h-px my-8 bg-black border-0" />
+			<div className="flex items-center justify-between p-1">
+				<p className="text-gray-400">{product.item_code}</p>
+				<p className="text-gray-400">{product.bar_code}</p>
 			</div>
 		</div>
 	);
