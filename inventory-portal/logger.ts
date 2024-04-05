35a50d1b16df6670e3cfa17f0a8a567d270a5afa
Yam Ng <108157094+YamNg@users.noreply.github.com>
2024-04-05 18:34:05 -0400

    remove export log file configuration

--- inventory-portal/logger.ts
+++ inventory-portal/logger.ts
@@ -12,14 +12,7 @@ const logger = winston.createLogger({
     winston.format.align(),
     winston.format.json(),
   ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: './logs/error.log',
-      level: 'error',
-    }),
-    new winston.transports.File({ filename: './logs/all.log' }),
-  ],
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
