8953e23953f2b4e052f586c57e6f8975705c3ca1
Yam Ng <108157094+YamNg@users.noreply.github.com>
2024-04-05 17:18:11 -0400

    fix mobile scroll issue

--- inventory-portal/src/app/inventory/page.tsx
+++ inventory-portal/src/app/inventory/page.tsx
@@ -4,36 +4,32 @@ import { fetchInventoryTotal } from "@/lib/data";
 import { InventorySortType } from "@/lib/definitions";
 
 export default async function Inventory({
-	searchParams
+  searchParams,
 }: {
-	searchParams?: {
-		query?: string;
-		page?: string;
-		sort?: InventorySortType;
-	};
+  searchParams?: {
+    query?: string;
+    page?: string;
+    sort?: InventorySortType;
+  };
 }) {
-	const query = searchParams?.query || "";
-	const currentPage = Number(searchParams?.page) || 1;
-	const sort = searchParams?.sort || undefined;
-	const [totalPages, totalCount] = await fetchInventoryTotal(query);
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const sort = searchParams?.sort || undefined;
+  const [totalPages, totalCount] = await fetchInventoryTotal(query);
 
-	return (
-		<div className="bg-gray-200 h-[90%] overflow-auto">
-			<div className="flex flex-col gap-5 mx-5">
-				<div className="flex justify-center mt-2">
-					<p className="text-lg">{totalCount} items found</p>
-				</div>
-				<div className="grow">
-					<InventoryList
-						query={query}
-						page={currentPage}
-						sort={sort}
-					/>
-				</div>
-				<div className="flex self-end w-full justify-center mb-2">
-					<Pagination totalPages={totalPages} />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-gray-200 h-[90%] min-h-[93vh]">
+      <div className="flex flex-col gap-5 mx-5">
+        <div className="flex justify-center mt-2">
+          <p className="text-lg">{totalCount} items found</p>
+        </div>
+        <div className="grow">
+          <InventoryList query={query} page={currentPage} sort={sort} />
+        </div>
+        <div className="flex self-end w-full justify-center mb-2">
+          <Pagination totalPages={totalPages} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- inventory-portal/src/app/layout.tsx
+++ inventory-portal/src/app/layout.tsx
@@ -23,10 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(
-          "h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
+        className={cn("bg-background font-sans antialiased", fontSans.variable)}
       >
         <NavBar />
         {children}
